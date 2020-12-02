fd830acaef9a0a6fbed7432f97759ea3398517f7
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-12-02 16:23:23 +0100

    refactor variable name and unnecessary box-unbox

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,10 @@
 public class StringCalculator {
 
-    public int add(String s) {
-        if(s.isEmpty()) {
+    public int add(String input) {
+        if (input.isEmpty()) {
             return 0;
         } else {
-            return Integer.valueOf(s);
+            return Integer.parseInt(input);
         }
     }
 }
