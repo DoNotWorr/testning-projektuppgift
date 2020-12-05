19b98dd1a5d7f3168ccc424d27f330854f4a9874
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-12-05 15:01:00 +0100

    refactor to stream to try it out

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     /**
@@ -7,10 +11,12 @@ public class StringCalculator {
      * @return sum of numbers
      */
     public int add(String input) {
+        String delimiter = ",";
         if (input.isEmpty()) {
             return 0;
         }
-        String[] terms = input.split(",");
+        //https://www.baeldung.com/java-string-with-separator-to-list
+        List<String> terms = Stream.of(input.split(delimiter)).collect(Collectors.toList());
 
         int sum = 0;
         for (String term : terms) {
