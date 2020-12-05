575dc22e596386b6f4a297fd814ced094ee350a2
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-12-05 15:28:20 +0100

    refactor: simplified stream

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,12 +17,7 @@ public class StringCalculator {
             return 0;
         }
         //https://www.baeldung.com/java-string-with-separator-to-list
-        List<String> terms = Stream.of(input.split(delimiter)).collect(Collectors.toList());
-
-        int sum = 0;
-        for (String term : terms) {
-            sum += Integer.parseInt(term);
-        }
-        return sum;
+        //https://www.baeldung.com/java-8-collectors
+        return Stream.of(input.split(delimiter)).mapToInt(Integer::parseInt).sum();
     }
 }
