ab468ec358c0b7dd9ea90ab6d3be12f1564f20d4
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-12-06 17:00:54 +0100

    refactor: replace alternative delimiter before concatination

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -11,14 +11,18 @@ public class StringCalculator {
     public int add(String input) {
         String defaultDelimiter = ",";
         String alternativeDelimiter = "\n";
+
         if (input.isEmpty()) {
             return 0;
         }
 
         //https://www.baeldung.com/java-string-with-separator-to-list
         //https://www.baeldung.com/java-8-collectors
-        //Replace alternative delimiter with default delimiter before split. Converts to int stream and summarizes
-        return Stream.of((input + ",0").replace(alternativeDelimiter, defaultDelimiter).split(defaultDelimiter))
+
+        //Replace alternative delimiter with default delimiter
+        //Concat ",0" to make sure split() never deletes all trailing empty strings
+        //Split and map to int stream, then summarize
+        return Stream.of((input.replace(alternativeDelimiter, defaultDelimiter).concat(",0")).split(defaultDelimiter))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
